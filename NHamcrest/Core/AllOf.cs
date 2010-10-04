e7477bf50f66e4290b1a5e77b6c22a182723890b
Graham <grahamrhay@gmail.com>
2010-10-04 22:59:29 +0100

    AllOf matcher

---
+++ NHamcrest/Core/AllOf.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NHamcrest.Core
+{
+    public class AllOf<T> : DiagnosingMatcher<T>
+    {
+        private readonly IEnumerable<IMatcher<T>> matchers;
+
+        public AllOf(IEnumerable<IMatcher<T>> matchers)
+        {
+            this.matchers = matchers;
+        }
+
+        protected override bool Matches(T item, IDescription mismatchDescription)
+        {
+            var failingMatchers = matchers.Where(matcher => matcher.Matches(item) == false);
+
+            foreach (var matcher in failingMatchers)
+            {
+                mismatchDescription.AppendDescriptionOf(matcher).AppendText(" ");
+                matcher.DescribeMismatch(item, mismatchDescription);
+                return false;
+            }
+
+            return true;
+        }
+
+        public override void DescribeTo(IDescription description)
+        {
+            description.AppendList("(", " " + "and" + " ", ")", matchers.Cast<ISelfDescribing>());
+        }
+    }
+}
---
+++ NHamcrest/Matches.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using NHamcrest.Core;
+
+namespace NHamcrest
+{
+    public static class Matches
+    {
+        public static IMatcher<T> AllOf<T>(IEnumerable<IMatcher<T>> matchers)
+        {
+            return new AllOf<T>(matchers);
+        }
+    }
+}
