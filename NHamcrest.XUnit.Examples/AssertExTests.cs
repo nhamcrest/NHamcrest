4347f0874e75c524de5e2c9977329de674617074
unknown <graham@.(none)>
2011-10-08 17:43:44 +0100

    NHamcrest.XUnit

---
+++ NHamcrest.XUnit.Examples/AssertExTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NHamcrest.Core;
+using Xunit;
+
+namespace NHamcrest.XUnit.Examples
+{
+    public class AssertExTests
+    {
+        [Fact]
+        public void Pass()
+        {
+            AssertEx.That(1, Is.EqualTo(1));
+        }
+
+        [Fact]
+        public void Fail()
+        {
+            AssertEx.That(1, Is.EqualTo(3));
+        }
+
+        [Fact]
+        public void One_more()
+        {
+            AssertEx.That(() => { throw new InvalidOperationException(); }, Throws.An<AccessViolationException>());
+        }
+    }
+}
